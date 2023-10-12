c95e3f4f51c2ad165c22a6d78f06b18933cc5d27
Mayuri Achewad <mayuriachewad@gmail.com>
2023-10-12 22:31:52 +0530

    first commit

---
+++ P5/data.js
@@ -0,0 +1,7 @@
+document.write("I am Mayuri Achewad")
+document.write("I am in internatinal zone")
+
+
+document.getElementById("fname").value;
+document.getElementById("lname").value;
+document.getElementById("fullname").innerHTML
